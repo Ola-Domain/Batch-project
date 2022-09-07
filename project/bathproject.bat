5cf84feaa1f3b886a924384c0573e466558e3786
Sulaimon Olasupo <110992426+Ola-Domain@users.noreply.github.com>
2022-09-07 03:14:06 -0700

    All file attach

---
+++ project/bathproject.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Press any key to continue with opening the test
+pause.......!
+
+C:\Users\DELL\Desktop\Ola-Domain.txt
+
+pause..........!!!
+
+C:\Users\DELL\Desktop\New.txt
+
+pause............!!!
